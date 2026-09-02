2f441bfb8dbe563c43db37b1879599882ad77ba1
Nivethithaprasanna <nivethithasri001@gmail.com>
2026-09-02 22:05:40 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,123 @@
+'''#arithmetic operators
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+print("add:",a+b)
+print("sub:",a-b)
+print("multi:",a*b)
+print("div:",a/b)
+print("modulus:",a%b)
+print("floordiiv:",a//b)
+print("expo:",a**b)'''
+
+'''#last digit
+num=int(input("enter the num:"))
+last_digit= num %10
+print("last digit:",last_digit)'''
+
+'''#remove last digit
+num=int(input("enter a 3 digit number:"))
+result=num//10
+print("after removing last digit:",result)'''
+
+'''#area and perimeter
+length=float(input("enter length:"))
+breadth=float(input("enter breadth:"))
+area=length*breadth
+perimeter=2*(length+breadth)
+print("area",area)
+print("perimeter",perimeter)'''
+
+'''#comparison
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+print("equal:",a==b)
+print("greater:",a>b)
+print("smaller:",a<b)'''
+
+
+'''#students markes
+mark=int(input("enter the students mark:"))
+print("mark is greater than or equal to 50:",mark>=50)'''
+
+'''#logical operators
+age=int(input("enter age:"))
+valid_id=input("have validid( yes/no):")
+print(age>=18 and valid_id=="yes")'''
+
+'''#one number greater
+a=int(input("enter number:"))
+b=int(input("enter number:"))
+print(a>100,b>100)'''
+
+'''#username and password
+username=input("enter usernmae:")
+password=input("enter password:")
+print(username=="nive" and password=="1516")'''
+
+'''#assignmnet operators
+num=100
+num+=10print("after+=:",num)
+num-=20
+print("after-=:",num)'''
+
+'''#membership operators
+students=["ram","janu","priya"]
+name=input("enter the student name:")
+print(name in students)'''
+
+'''fruits=["apple","orange","strawberry"]
+print("apple" in fruits)
+print("orange" in fruits)'''
+
+'''#bitwise operators
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+print(a&b)
+print(a|b)
+print(a^b)'''
+
+'''#result for given number
+num=int(input("enter a number:"))
+print("bitwise not:",~num)'''
+
+'''#IF-ELSE
+num=int(input("enter a number:"))
+if num%2==0:
+    print("even")
+else:
+    print("odd")'''
+
+#positive, negative or zero
+num=int(input("enter a number:"))
+if num>0:
+  print("positive")
+elif num<0:
+  print("negative")
+else:
+  print("zero")
+                      
+          
+
+
+
+      
+
+         
+
+          
+         
+         
+      
+      
+     
+     
+
+
+
+
+
+             
+             
+
+
+         
